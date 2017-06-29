c1ad21500327685f1953d6e6e02b7fe586058486
Goyoonho <rhdbsgh0629@naver.com>
2017-06-29 18:36:05 +0900

    models

---
+++ server/app/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+class UniversityAdmin(admin.ModelAdmin):
+    model = University
+    list_display = ('name', )
+
+
+class UndergraduateAdmin(admin.ModelAdmin):
+    model = Undergraduate
+    list_display = ('name', 'university',)
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    model = Department
+    list_display = ('name', 'undergraduate',)
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    model = Faculty
+    list_display = ('name', 'age', 'email', 'phone', 'belong',)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+    list_display = ('name', 'faculty',)
+
+
+
+class CourseEvaluationAdmin(admin.ModelAdmin):
+    model = CourseEvaluation
+    list_display = ('course', 'pnc', 'description',)
+
+
+class EvaluationRecordAdmin(admin.ModelAdmin):
+    model = EvaluationRecord
+    list_display = ('evaluation', 'user', 'score',)
+    
+
+# UserAdmin 재등록
+admin.site.register(University, UniversityAdmin)
+admin.site.register(Undergraduate, UndergraduateAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseEvaluation, CourseEvaluationAdmin)
+admin.site.register(EvaluationRecord, EvaluationRecordAdmin)
---
+++ server/app/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+class University(models.Model):
+    name = models.CharField(
+        verbose_name='학교명', default='', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Undergraduate(models.Model):
+    name = models.CharField(
+        verbose_name='학부명', default='', max_length=50)
+    university = models.ForeignKey(University, verbose_name='학교',
+                                   on_delete=models.DO_NOTHING, null=True)
+
+    def __str__(self):
+        return ' '.join([self.university.__str__(), self.name])
+
+
+class Department(models.Model):
+    name = models.CharField(
+        verbose_name='학과명', default='', max_length=50)
+    undergraduate = models.ForeignKey(Undergraduate, verbose_name='학부',
+                                      on_delete=models.DO_NOTHING, null=True)
+
+    def __str__(self):
+        return ' '.join([self.undergraduate.__str__(), self.name])
+
+
+class Faculty(models.Model):
+    photo = models.ImageField(
+        verbose_name='사진', null=True, blank=True)
+    name = models.CharField(
+        verbose_name='교수명', default='', max_length=50)
+    age = models.PositiveSmallIntegerField(verbose_name='나이', null=True)
+    email = models.EmailField(verbose_name='메일', default='', blank=True)
+    phone = models.CharField(
+        verbose_name='전화번호', default='', max_length=20)
+    belong = models.ForeignKey(
+        Department, verbose_name='소속', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(
+        verbose_name='강의명', max_length=20)
+    faculty = models.ForeignKey(
+        Faculty, verbose_name='강사', on_delete=models.DO_NOTHING)
+    info = models.TextField(
+        verbose_name='설명', blank=True, null=True, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class CourseEvaluation(models.Model):
+    course = models.ForeignKey(
+        Course, verbose_name='강의', on_delete=models.DO_NOTHING)
+    pnc = models.BooleanField(default=True)
+    description = models.CharField(verbose_name='평가내용', max_length=20)
+
+    def __str__(self):
+        return self.description
+
+
+class EvaluationRecord(models.Model):
+    evaluation = models.ForeignKey(CourseEvaluation, verbose_name='평가내용')
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    score = models.PositiveSmallIntegerField(verbose_name='점수', default=1,
+                                             validators=[MaxValueValidator(5), MinValueValidator(1)])
+
+    def __str__(self):
+        return self.evaluation.__str__()
